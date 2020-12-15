2ab40d7ea1081dc1b2dba1df1230e0c32e73177b
s1lentalarm <nsilentalarm@gmail.com>
2020-12-16 00:58:06 +0300

    lol

---
+++ vendor/github.com/booking_room/scr/dbhandler.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ReserveRow struct {
+	ID           int
+	NickName     string
+	ClubName     string
+	PeopleNumber int
+	ReserveTime  int
+	ReserveDate  string
+}
+
+func openDB(db_name string, db_path string) *sql.DB {
+	db, err := sql.Open(db_name, db_path)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func insertReserve(db *sql.DB, table string, nickname string, clubname string, people_number int, reserv_time int, reserv_date string) error {
+	_, err := db.Exec(
+		"INSERT INTO "+table+" (nickname, clubname, people_number, reserv_time, reserv_date) values ($1, $2, $3, $4, $5)",
+		nickname, clubname, people_number, reserv_time, reserv_date)
+	if err != nil {
+		panic(err)
+		return err
+	}
+	return nil
+}
+
+func getDBRows(db *sql.DB, table string) (*sql.Rows, error) {
+	rows, err := db.Query("SELECT * FROM " + table)
+	if err != nil {
+		return nil, err
+	}
+
+	return rows, nil
+}
+
+func getDateReserves(db *sql.DB, table string, targetDate string) ([]ReserveRow, error) {
+	rows, err := getDBRows(db, table)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	timeReserves := []ReserveRow{}
+
+	for rows.Next() {
+		Row := ReserveRow{}
+		err := rows.Scan(&Row.ID, &Row.NickName, &Row.ClubName, &Row.PeopleNumber, &Row.ReserveTime, &Row.ReserveDate)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if Row.ReserveDate == targetDate {
+			timeReserves = append(timeReserves, Row)
+		}
+	}
+	return timeReserves, nil
+}
+
+func reserveIsExist(db *sql.DB, table, date string, time int) bool {
+	timeRes, _ := getDateReserves(db, table, date)
+
+	for _, value := range timeRes {
+		if value.ReserveTime == time {
+			return true
+		}
+	}
+	return false
+}
+
+func deleteOldReserves(db *sql.DB, table []string, date string) {
+	for _, target := range table {
+		_, err := db.Exec("DELETE FROM " + target + " WHERE reserv_date < '" + date + "'")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func tryDeleteRowByOwner(db *sql.DB, table string, date string, userName string, time string) {
+	_, err := db.Exec("DELETE FROM " + table + " WHERE nickname='" + userName + "' AND reserv_date='" + date + "' AND reserv_time=" + time)
+	if err != nil {
+		panic(err)
+	}
+}
