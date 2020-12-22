b89f18fa332b36fb7c1665734d6724c870d3e03e
s1lentalarm <nsilentalarm@gmail.com>
2020-12-22 22:35:14 +0300

    фикс таблицы клуба

--- scr/clubs/clubs.go
+++ scr/clubs/clubs.go
@@ -25,23 +25,23 @@ func Clubs(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	clubs, _ := dbh.GetClubs(db, true)
-	member := dbh.IsUserInClub(db, user.Name, user.ID)
+	//member := dbh.IsUserInClub(db, user.Name, user.ID)
 
 	tmpl, _ := template.ParseFiles("static/clubs.html")
 	if r.Method != http.MethodPost {
 		dataMap := map[string]interface{}{
-			"user":   user,
-			"clubs":  clubs,
-			"inclub": member,
+			"user":  user,
+			"clubs": clubs,
+			//"inclub": member,
 		}
 		_ = tmpl.Execute(w, dataMap)
 		return
 	}
 
-	if member == false {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
+	//if member == false {
+	//	http.Redirect(w, r, "/", http.StatusFound)
+	//	return
+	//}
 
 	clubName := r.FormValue("clubName")
 	date := time.Now().Format("02.01.2006")
