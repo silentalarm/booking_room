87abd8722f7582ca4fd8b937d7f1d1656aee8d32
s1lentalarm <nsilentalarm@gmail.com>
2020-12-24 17:38:32 +0300

    фикс шапки

--- js/scripts.js
+++ js/scripts.js
@@ -167,6 +167,24 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+// Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
 
  // заполнение select
  //$("select option[value=" + val + "]").attr('selected', 'true').text(text);
