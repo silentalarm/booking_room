634d55b1b21c204e5067e202bf60e22a043abf55
s1lentalarm <nsilentalarm@gmail.com>
2020-12-27 18:11:13 +0300

    новый стиль моих клубов

--- scr/clubs/myclubs.go
+++ scr/clubs/myclubs.go
@@ -5,9 +5,10 @@ import (
 	ses "github.com/silentalarm/booking_room/scr/sessions"
 	"html/template"
 	"net/http"
+	"sort"
 )
 
-func MyClubs(w http.ResponseWriter, r *http.Request) {
+func MyClubs1(w http.ResponseWriter, r *http.Request) {
 	session, err := ses.Store.Get(r, "auth-session")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -47,4 +48,58 @@ func MyClubs(w http.ResponseWriter, r *http.Request) {
 	clubName := r.FormValue("clubName")
 	dbh.UserLeaveСlub(db, user.Name, user.ID, clubName)
 	http.Redirect(w, r, "/myclubs", http.StatusFound)
+}
+
+func MyClubs(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+		return
+	}
+	redirect := "/"
+
+	session, err := ses.Store.Get(r, "auth-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	user := ses.GetUser(session)
+
+	if user.Authenticated == false {
+		http.Redirect(w, r, redirect, http.StatusFound)
+		return
+	}
+
+	db := dbh.OpenDB()
+	defer db.Close()
+
+	member := dbh.IsUserClubMember(db, user.Name, user.ID)
+	clubs, _ := dbh.GetUserClubs(db, true, user.Name, user.ID)
+	onConfirmationClubs, _ := dbh.GetUserClubs(db, false, user.Name, user.ID)
+	sort.Sort(BySize(clubs))
+	//member := dbh.IsUserClubMember(db, user.Name, user.ID)
+
+	tmpl, _ := template.ParseFiles("static/myClubs_v2.html")
+	if r.Method != http.MethodPost {
+		dataMap := map[string]interface{}{
+			"user":    user,
+			"clubs":   clubs,
+			"member":  member,
+			"onConCl": onConfirmationClubs,
+		}
+		_ = tmpl.Execute(w, dataMap)
+		return
+	}
+
+	clubName := r.FormValue("clubName")
+	sumbit := r.FormValue("sumbit")
+
+	switch sumbit {
+	case "goToClub":
+		redirect = clubPage(clubName)
+	case "leaveClub":
+		redirect = leave(db, user.Name, user.ID, clubName)
+	}
+
+	http.Redirect(w, r, redirect, http.StatusFound)
 }
